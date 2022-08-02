83c54a57b044901f74e4e244626bcf35eadc53cc
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2022-08-02 10:45:46 +0300

    alaaa,,inamuok

--- index.js
+++ index.js
@@ -8,24 +8,52 @@ class book{
 
 class UI {
     addBooktoList(book) {
+        
         const list = document.getElementById('book-list');
         const row = document.createElement('tr');
-        row.innerHTML = '';
+        row.innerHTML = '
         
-        <td>${book.title</td>
-        <td>${book.author</td>
-        <td>${book.isbn</td>
-        <td <a> href = "" class = "delete"> X </a> </td>
-        
+                            <td>${book.title}</td>
+                            <><td>${book.author}</td><td>${book.isbn}</td><td><a href="" class="delete">X</a></td></>
+
+        ';
+list.appendChild(row);
+    
+}
+
+showAlert(message, classname) {
+    const div = document.createElement('div');
+    div.className = 'alert ${className}';
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    },3000);
+    
+}
+
+deleteBook(target) {
+    if(target.className === 'delete') {
+        target.parentElement.parentElement.remove();
     }
 }
 
-document.getElementById(book).addEventListener('submit', function(e) {
+clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+}
+
+document.getElementById('book-form').addEventListener('submit', function(e) {
     const title = document.getElementById('title').value ;
     const author = document.getElementById('author').value;
     const isbn = document.getElementById('isbn').value;
 
-    const book = new book(title, author, isbn);
+    const book = new Book(title, author, isbn);
 
     const ui = new UI();
 
@@ -38,6 +66,22 @@ document.getElementById(book).addEventListener('submit', function(e) {
         ui.addBooktoList(book);
 
         ui.showAlert('Book added successfully', 'success');
+
+        ui.clearFields();
+
     }
 
-}
+    e.preventDefault();
+});
+
+document.getElementById('book-list').addEventListener('click',function(e) {
+    const ui = new UI();
+
+    ui.deleteBook(e.target);
+
+    ui.showAlert('Book removed successfully!!!', 'success');
+
+    e.preventDefault();
+
+});
+
